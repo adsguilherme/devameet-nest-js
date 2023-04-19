1a6ee45b450989d9e16e5f2759f9c8457cfa4d8c
gscode <adm.guilhermedesousa@gmail.com>
2023-04-19 15:26:38 -0300

    implementando execução da sale de reunião - p1

---
+++ src/room/helpers/roommessages.helper.ts
@@ -0,0 +1,6 @@
+export const RoomMessagesHelper = {
+  ROOM_LINK_NOT_FOUND: 'Reunião não encontrada.',
+  ROOM_MAX_USERS: 'A quantidade máxima de usuários por sala é de 10 pessoas.',
+  JOIN_USER_NOT_VALID: 'Usuário informado não é válido.',
+  LINK_NOT_VALID: 'Link informado não é válido.',
+};
